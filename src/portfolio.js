20cb8083e4b584327d5137f55051a407f8808567
Abdul Rehman <akabdulrehman159@gmail.com>
2026-03-03 01:23:17 +0500

    feat: update profile data

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,14 +26,14 @@ const greeting = {
     "Full Stack Developer specializing in architecting end-to-end, AI-driven products with Next.js, Python, and AWS. I thrive on building sustainable, scalable systems that bridge technical excellence with real-world social impact",
   resumeLink:
     "https://drive.google.com/file/d/1bXRknv_h-XI_3CQ3SGPteGODtvEb7YvI/view?usp=sharing",
-  portfolio_repository: "https://github.com/Abdul Rehman/masterPortfolio",
-  githubProfile: "https://github.com/Abdul Rehman",
+  portfolio_repository: "https://github.com/abdulrehman705/abdulrehman-portfolio",
+  githubProfile: "https://github.com/abdulrehman705",
 };
 
 const socialMediaLinks = [
   /* Your Social Media Link */
   // github: "https://github.com/abdulrehman705",
-  // linkedin: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
+  // linkedin: "https://www.linkedin.com/in/abdulrehman705/",
   // gmail: "akabdulrehman159@gmail.com",
   // gitlab: "https://gitlab.com/ashutoshhathidara98",
   // facebook: "https://www.facebook.com/laymanbrother.19/",
@@ -43,14 +43,14 @@ const socialMediaLinks = [
   {
     name: "Github",
     link: "https://github.com/abdulrehman705",
-    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
-    backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+    fontAwesomeIcon: "fa-github",
+    backgroundColor: "#181717",
   },
   {
     name: "LinkedIn",
-    link: "https://www.linkedin.com/in/abdulrehman705/",
-    fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
-    backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
+    link: "https://www.linkedin.com/in/abdulrehman705",
+    fontAwesomeIcon: "fa-linkedin-in",
+    backgroundColor: "#0077B5",
   },
   // {
   //   name: "YouTube",
@@ -61,26 +61,20 @@ const socialMediaLinks = [
   {
     name: "Gmail",
     link: "mailto:akabdulrehman159@gmail.com",
-    fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
-    backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
+    fontAwesomeIcon: "fa-google",
+    backgroundColor: "#D14836",
   },
-  // {
-  //   name: "X-Twitter",
-  //   link: "https://twitter.com/ashutosh_1919",
-  //   fontAwesomeIcon: "fa-x-twitter", // Reference https://fontawesome.com/icons/x-twitter?f=brands&s=solid
-  //   backgroundColor: "#000000", // Reference https://simpleicons.org/?q=x
-  // },
-  // {
-  //   name: "Facebook",
-  //   link: "https://www.facebook.com/laymanbrother.19/",
-  //   fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
-  //   backgroundColor: "#1877F2", // Reference https://simpleicons.org/?q=facebook
-  // },
+  {
+    name: "Facebook",
+    link: "https://www.facebook.com/laymanbrother.19/",
+    fontAwesomeIcon: "fa-facebook-f",
+    backgroundColor: "#1877F2",
+  },
   {
     name: "Instagram",
     link: "https://www.instagram.com/a_.rehman0/",
-    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
-    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
+    fontAwesomeIcon: "fa-instagram",
+    backgroundColor: "#E4405F",
   },
 ];
 
