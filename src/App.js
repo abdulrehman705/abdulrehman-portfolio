855a6207bdfa93a2098315d4fb32734165b2781d
Abdul Rehman <akabdulrehman159@gmail.com>
2026-03-12 16:37:42 +0500

    feat: add custom cursor and animation effect

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
 import { chosenTheme } from "./theme";
 import { GlobalStyles } from "./global";
+import CursorEffect from "./components/cursorEffect/CursorEffect";
 
 function App() {
   return (
     <ThemeProvider theme={chosenTheme}>
       <>
         <GlobalStyles />
+        <CursorEffect theme={chosenTheme} />
         <div>
           <Main theme={chosenTheme} />
         </div>
